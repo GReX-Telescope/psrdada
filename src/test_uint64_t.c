9146a77e049c6da557eecb6a0becc76089a8a335
Willem van Straten <wvanstra@users.sourceforge.net>
2005-02-23 13:48:12 +0000

    extra test

--- src/test_uint64_t.c
+++ src/test_uint64_t.c
@@ -7,7 +7,13 @@ int main ()
   char text[64];
 
   sprintf (text, "%llu\n", value);
-  sscanf (text, "%llu\n", &value);
+
+  value = 0;
+
+  if (sscanf (text, "%llu\n", &value) != 1) {
+    fprintf (stderr, "test_uint64_t: error parsing %%llu\n");
+    return -1;
+  }
 
   if (value != UINT64_MAX) {
     fprintf (stderr, "test_uint64_t: error parsing UINT64_MAX\n");
