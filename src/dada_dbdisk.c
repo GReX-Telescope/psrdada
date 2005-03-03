6a4c66e0d29ed61448ba6a90cae164cd4920bc07
Willem van Straten <wvanstra@users.sourceforge.net>
2005-03-03 17:20:18 +0000

    shell of dbdisk is growing

--- src/dada_dbdisk.c
+++ src/dada_dbdisk.c
@@ -18,6 +18,44 @@ void usage()
 	   " -A Adaptive sleep algorithm (slow virtual memory flushing)\n");
 }
 
+int main_loop (dada_t* dada,
+	       ipcio_t* data_block,
+	       ipcbuf_t* header_block,
+	       multilog_t* log)
+{
+  char* header = 0;
+  uint64_t header_size = 0;
+
+  char* buffer = 0;
+  uint64_t buffer_size = 0;
+
+  uint64_t bytes_read = 0;
+
+  /* Wait for the next valid header sub-block */
+  header = ipcbuf_get_next_read (header_block, &header_size);
+    
+  // get the header size and duplicate it
+
+  ipcbuf_mark_cleared (header_block);
+
+  while (!ipcbuf_eod((ipcbuf_t*)data_block)) {
+
+    bytes_read = ipcio_read (data_block, buffer, buffer_size);
+
+    if (bytes_read < 0) {
+      perror ("ipcio_read error");
+      return -1;
+    }
+
+  }
+
+  ipcio_reset (data_block);
+
+  return 0;
+
+}
+
+
 int main (int argc, char **argv)
 {
   /* DADA configuration */
@@ -61,42 +99,51 @@ int main (int argc, char **argv)
 
   log = multilog_open ("dada_dbdisk", daemon);
 
-  /* set up for daemon usage */	  
   if (daemon) {
 
+    /* set up for daemon usage */	  
     if (fork() < 0)
       exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 
   }
-
+  else
+    multilog_add (log, stderr);
 
   multilog_serve (log, dada.log_port);
 
   /* First connect to the shared memory */
-  if (ipcio_connect (&data_block, dada.data_key) < 0) {
-    multilog (log, LOG_ERR, "Failed to connect to data block\n");
-    return EXIT_FAILURE;
-  }
-
-  if (ipcbuf_lock_write ((ipcbuf_t*)&data_block) < 0) {
-    multilog (log, LOG_ERR, "Could not lock designated writer status\n");
-    return EXIT_FAILURE;
-  }
-
   if (ipcbuf_connect (&header_block, dada.hdr_key) < 0) {
     multilog (log, LOG_ERR, "Failed to connect to header block\n");
     return EXIT_FAILURE;
   }
 
-  if (ipcbuf_lock_write (&header_block) < 0) {
+  if (ipcbuf_lock_read (&header_block) < 0) {
     multilog (log, LOG_ERR, "Could not lock designated writer status\n");
     return EXIT_FAILURE;
   }
 
-  /* Disconnect to the shared memory */
-  if (ipcbuf_unlock_write ((ipcbuf_t*)&data_block) < 0) {
+  if (ipcio_connect (&data_block, dada.data_key) < 0) {
+    multilog (log, LOG_ERR, "Failed to connect to data block\n");
+    return EXIT_FAILURE;
+  }
+
+  if (ipcio_open (&data_block, 'R') < 0) {
+    multilog (log, LOG_ERR, "Failed to open data block for reading\n");
+    return EXIT_FAILURE;
+  }
+
+
+  while (!state.quit) {
+
+    if (main_loop (dada, &data_block, &header_block, log) < 0)
+      multilog (log, LOG_ERR, "Error during transfer\n");
+
+  }
+
+  /* Disconnect from the shared memory */
+  if (ipcio_close (&data_block) < 0) {
     multilog (log, LOG_ERR, "Could not unlock designated writer status\n");
     return EXIT_FAILURE;
   }
