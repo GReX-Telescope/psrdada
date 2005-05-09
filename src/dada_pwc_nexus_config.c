4a54610245dd1b9b957763eb61de7fa16fede926
Willem van Straten <wvanstra@users.sourceforge.net>
2005-05-09 19:27:24 +0000

    send parsed headers to nodes

--- src/dada_pwc_nexus_config.c
+++ src/dada_pwc_nexus_config.c
@@ -1,14 +1,23 @@
 #include "dada_pwc_nexus.h"
 #include "futils.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 /*! parse a configuration into unique headers for each primary write client */
 int dada_pwc_nexus_header_parse (dada_pwc_nexus_t* n, const char* buffer);
 
 int dada_pwc_nexus_config (void* context, FILE* output, char* args)
 {
   dada_pwc_nexus_t* nexus = (dada_pwc_nexus_t*) context;
+  dada_node_t* node = 0;
+
+  unsigned inode, nnode = nexus_get_nnode ((nexus_t*) nexus);
+
   char* filename = args;
   char* buffer = 0;
+  char* hdr = 0;
+
   long fsize = 0;
   int error = 0;
 
@@ -17,8 +26,20 @@ int dada_pwc_nexus_config (void* context, FILE* output, char* args)
     return -1;
   }
 
+  if (!args) {
+    fprintf (output, "Please specify config filename\n");
+    return -1;
+  }
+
+  filename = strsep (&args, " \t\n\r");
+
+  if (!filename || filename[0] == '\0') {
+    fprintf (output, "Please specify config filename\n");
+    return -1;
+  } 
+
 #ifdef _DEBUG
-  fprintf (stderr, "dada_pwc_nexus_config fopen (%s, 'r')\n", args);
+  fprintf (stderr, "dada_pwc_nexus_config fopen (%s, 'r')\n", filename);
 #endif
 
   fsize = filesize (filename);
@@ -38,6 +59,24 @@ int dada_pwc_nexus_config (void* context, FILE* output, char* args)
   error = dada_pwc_nexus_header_parse (nexus, buffer);
   free (buffer);
 
-  return error;
+  if (error)
+    return error;
 
+  for (inode=0; inode < nnode; inode++) {
+
+    node = (dada_node_t*) nexus->nexus.nodes[inode];
+    strcpy (nexus->pwc->header, "header ");
+    strcat (nexus->pwc->header, node->header);
+
+    hdr = nexus->pwc->header;
+    /* replace new line with \\ */
+    while ( (hdr = strchr(hdr, '\n')) != 0 )
+      *hdr = '\\';
+
+    nexus_send_node ((nexus_t*) nexus, inode, nexus->pwc->header);
+
+  }
+
+  return dada_pwc_command_ack (nexus->pwc, dada_pwc_prepared);
 }
+
