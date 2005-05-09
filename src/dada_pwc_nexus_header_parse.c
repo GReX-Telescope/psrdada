090c00e018a2196fd6923328f2ee412262ab69a6
Willem van Straten <wvanstra@users.sourceforge.net>
2005-05-09 19:27:33 +0000

    fixed segfault

--- src/dada_pwc_nexus_header_parse.c
+++ src/dada_pwc_nexus_header_parse.c
@@ -1,5 +1,6 @@
 #include "dada_pwc_nexus.h"
 #include "ascii_header.h"
+#include "futils.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -11,7 +12,7 @@ int dada_pwc_config_header (const char* prefix, char* header,
   char parm_value [64] = "";
 
   char* parms[] = { "FREQ", "BW", 0 };
-  char* parm = 0;
+  char** parm = 0;
 
   if (!config) {
     fprintf (stderr, "dada_pwc_config_header no config");
@@ -22,16 +23,16 @@ int dada_pwc_config_header (const char* prefix, char* header,
     return -1;
   }
 
-  for (parm=parms[0]; parm!=0; parm++) {
+  for (parm=parms; *parm != 0; parm++) {
 
-    sprintf (parm_name, "%s%s", prefix, parm);
+    sprintf (parm_name, "%s%s", prefix, *parm);
 
     if (ascii_header_get (config, parm_name, "%s", parm_value) < 0)
       fprintf (stderr, "dada_pwc_config_header WARNING %s not found\n",
 	       parm_name);
-    else if (ascii_header_set (header, parm, "%s", parm_value) < 0) {
+    else if (ascii_header_set (header, *parm, "%s", parm_value) < 0) {
       fprintf (stderr, "dada_pwc_config_header ERROR setting %s=%s\n",
-	       parm,parm_value);
+	       *parm,parm_value);
       return -1;
     }
       
@@ -49,21 +50,24 @@ int dada_pwc_nexus_header_parse (dada_pwc_nexus_t* n, const char* buffer)
   unsigned inode, nnode = nexus_get_nnode ((nexus_t*) n);
 
   /* First set up the common header parameters */
-  strcpy (n->working_header, n->header_template);
+  if (n->header_template)
+    fileread (n->header_template, n->pwc->header, n->pwc->header_size);
+  else
+    n->pwc->header[0] = '\0';
 
-  if (dada_pwc_config_header (node_name, n->working_header, buffer) < 0)
+  if (dada_pwc_config_header (node_name, n->pwc->header, buffer) < 0)
     return -1;
 
   for (inode=0; inode < nnode; inode++) {
 
     node = (dada_node_t*) n->nexus.nodes[inode];
-    if (node->header_size < n->header_size) {
-      node->header = realloc (node->header, n->header_size);
-      node->header_size = n->header_size;
+    if (node->header_size < n->pwc->header_size) {
+      node->header = realloc (node->header, n->pwc->header_size);
+      node->header_size = n->pwc->header_size;
     }
 
-    strcpy (node->header, n->working_header);
-    sprintf (node_name, "Band%2d", inode);
+    strcpy (node->header, n->pwc->header);
+    sprintf (node_name, "Band%d_", inode);
     if (dada_pwc_config_header (node_name, node->header, buffer) < 0)
       return -1;
     
