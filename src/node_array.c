c2601c59b4df4588ec885e3389d0b3ebf290898f
Willem van Straten <straten@users.sourceforge.net>
2005-05-16 06:19:11 +0000

    for managing an array of connected sockets

---
+++ src/node_array.c
@@ -0,0 +1,109 @@
+#include "node_array.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+
+// #define _DEBUG 1
+
+/*! Create a new node array */
+node_array_t* node_array_create ()
+{
+  node_array_t* array = (node_array_t*) malloc (sizeof(node_array_t));
+  assert (array != 0);
+
+  array -> nodes = 0;
+  array -> nnode = 0;
+  array -> cnode = 0;
+
+  pthread_mutex_init(&(array->mutex), NULL);
+
+  return array;
+}
+
+/*! Destroy a node array */
+int node_array_destroy (node_array_t* array)
+{
+  if (!array)
+    return -1;
+
+  if (array->nodes)
+    free (array->nodes);
+
+  pthread_mutex_destroy (&(array->mutex));
+
+  return 0;
+}
+
+/*! Add a node to the array */
+int node_array_add (node_array_t* array, char* name)
+{
+  /* pointer to new node_t structure in array */
+  node_t* new_node = 0;
+
+  /* array counter */
+  unsigned inode;
+
+  pthread_mutex_lock (&(array->mutex));
+
+  /* ensure that each node in array is a unique device */
+  for (inode = 0; inode < array->nnode; inode++) {
+    if (!strcmp (name, array->nodes[inode].name)) {
+      fprintf (stderr, "node_array_add: %s is already in array\n", name);
+      pthread_mutex_unlock (&(array->mutex));
+      return -1;
+    }
+  }
+
+  array->nodes = realloc (array->nodes, (array->nnode+1)*sizeof(node_t));
+  assert (array->nodes != 0);
+
+  new_node = array->nodes + array->nnode;
+  array->nnode ++;
+
+  new_node->name = strdup (name);
+  assert (new_node->name != 0);
+
+  pthread_mutex_unlock (&(array->mutex));
+
+  return 0;
+}
+
+/*! Get the available amount of node space */
+uint64_t node_array_get_available (node_array_t* array)
+{
+  uint64_t available_space = 0;
+  unsigned inode;
+
+  pthread_mutex_lock (&(array->mutex));
+
+  for (inode = 0; inode < array->nnode; inode++)
+    available_space += array->nodes[inode].space;
+
+  pthread_mutex_unlock (&(array->mutex));
+
+  return available_space;
+}
+
+/*! Open a file on the node array, return the open file descriptor */
+int node_array_open (node_array_t* array, uint64_t filesize,
+		     uint64_t* optimal_buffer_size)
+{
+  int fd;
+
+  pthread_mutex_lock (&(array->mutex));
+  array->cnode %= array->nnode;
+  fd = array->nodes[array->cnode].fd;
+  array->cnode ++;
+  pthread_mutex_unlock (&(array->mutex));
+
+  return fd;
+
+}
---
+++ src/node_array.h
@@ -0,0 +1,59 @@
+#ifndef __DADA_NODE_ARRAY_H
+#define __DADA_NODE_ARRAY_H
+
+/* ************************************************************************
+
+   ************************************************************************ */
+
+#include <pthread.h>
+#include <inttypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  typedef struct {
+
+    int      fd;       /* file descriptor of socket connected to node */
+    char*    name;     /* hostname of node */
+    uint64_t space;    /* number of bytes space left on node */
+
+  } node_t;
+
+  typedef struct {
+    
+    node_t*  nodes;    /* nodes to which data will be written */
+    unsigned nnode;    /* number of nodes */
+    unsigned cnode;    /* the current node */
+
+    /* for multi-threaded use of the dbnode struct */
+    pthread_mutex_t mutex;
+    
+  } node_array_t;
+
+  /*! Create a new node array */
+  node_array_t* node_array_create ();
+
+  /*! Destroy a node array */
+  int node_array_destroy (node_array_t*);
+
+  /*! Add a node to the array */
+  int node_array_add (node_array_t*, char* path);
+
+  /*! Enable/disable file overwriting */
+  int node_array_set_overwrite (node_array_t* array, char value);
+
+  /*! Return the open file descriptor of a node in the array */
+  int node_array_open (node_array_t*, uint64_t size, uint64_t* bs);
+
+  /*! Get the total amount of node space */
+  uint64_t node_array_get_total (node_array_t*);
+
+  /*! Get the available amount of node space */
+  uint64_t node_array_get_available (node_array_t*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
