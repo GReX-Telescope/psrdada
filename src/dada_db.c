75015f900f29a971390fdd49feb871c26a4b2519
Willem van Straten <straten@users.sourceforge.net>
2007-10-25 01:08:49 +0000

    can optionally create DADA Header and Data Blocks with specified key

--- src/dada_db.c
+++ src/dada_db.c
@@ -10,14 +10,17 @@ void usage ()
 {
   fprintf (stdout,
 	   "dada_db - create or destroy the DADA shared memory ring buffer\n"
-
-	   "USAGE: dada_db [-d] [-n nbufs] [-b bufsz]\n"
+	   "\n"
+	   "USAGE: dada_db [-d] [-k key] [-n nbufs] [-b bufsz]\n"
 	   "WHERE:\n"
+	   " -k  hexadecimal shared memory key  [default: %x]\n"
 	   " -n  number of buffers in ring      [default: %"PRIu64"]\n"
 	   " -b  size of each buffer (in bytes) [default: %"PRIu64"]\n"
 	   " -d  destroy the shared memory area [default: create]\n"
 	   " -l  lock the shared memory area in physical RAM\n", 
-	   DADA_DEFAULT_BLOCK_NUM, DADA_DEFAULT_BLOCK_SIZE);
+	   DADA_DEFAULT_BLOCK_KEY,
+	   DADA_DEFAULT_BLOCK_NUM,
+	   DADA_DEFAULT_BLOCK_SIZE);
 }
 
 int main (int argc, char** argv)
@@ -26,18 +29,16 @@ int main (int argc, char** argv)
   uint64_t bufsz = DADA_DEFAULT_BLOCK_SIZE;
   uint64_t hdrsz = DADA_DEFAULT_HEADER_SIZE;
 
-  key_t data_key = DADA_DATA_BLOCK_KEY;
-  key_t hdr_key  = DADA_HEADER_BLOCK_KEY;
+  key_t dada_key = DADA_DEFAULT_BLOCK_KEY;
 
   ipcbuf_t data_block = IPCBUF_INIT;
   ipcbuf_t header = IPCBUF_INIT;
-
 
   int destroy = 0;
   int lock = 0;
   int arg;
 
-  while ((arg = getopt(argc, argv, "hdn:b:l")) != -1) {
+  while ((arg = getopt(argc, argv, "hdk:n:b:l")) != -1) {
 
     switch (arg)  {
     case 'h':
@@ -48,12 +49,25 @@ int main (int argc, char** argv)
       destroy = 1;
       break;
 
+    case 'k':
+      if (sscanf (optarg, "%x", &dada_key) != 1) {
+	fprintf (stderr, "dada_db: could not parse key from %s\n", optarg);
+	return -1;
+      }
+      break;
+
     case 'n':
-      sscanf (optarg, "%"PRIu64"", &nbufs);
+      if (sscanf (optarg, "%"PRIu64"", &nbufs) != 1) {
+	fprintf (stderr, "dada_db: could not parse nbufs from %s\n", optarg);
+	return -1;
+      }
       break;
 
     case 'b':
-      sscanf (optarg, "%"PRIu64"", &bufsz);
+      if (sscanf (optarg, "%"PRIu64"", &bufsz) != 1) {
+	fprintf (stderr, "dada_db: could not parse bufsz from %s\n", optarg);
+	return -1;
+      }
       break;
 
     case 'l':
@@ -64,10 +78,10 @@ int main (int argc, char** argv)
 
   if (destroy) {
 
-    ipcbuf_connect (&data_block, data_key);
+    ipcbuf_connect (&data_block, dada_key);
     ipcbuf_destroy (&data_block);
 
-    ipcbuf_connect (&header, hdr_key);
+    ipcbuf_connect (&header, dada_key + 1);
     ipcbuf_destroy (&header);
 
     fprintf (stderr, "Destroyed DADA data and header blocks\n");
@@ -75,7 +89,7 @@ int main (int argc, char** argv)
     return 0;
   }
 
-  if (ipcbuf_create (&data_block, data_key, nbufs, bufsz) < 0) {
+  if (ipcbuf_create (&data_block, dada_key, nbufs, bufsz) < 0) {
     fprintf (stderr, "Could not create DADA data block\n");
     return -1;
   }
@@ -83,7 +97,7 @@ int main (int argc, char** argv)
   fprintf (stderr, "Created DADA data block with"
 	   " nbufs=%"PRIu64" bufsz=%"PRIu64"\n", nbufs, bufsz);
 
-  if (ipcbuf_create (&header, hdr_key, 1, hdrsz) < 0) {
+  if (ipcbuf_create (&header, dada_key + 1, 1, hdrsz) < 0) {
     fprintf (stderr, "Could not create DADA header block\n");
     return -1;
   }
