769c75ed7b48b5f8d47b9e461e61a6c533e18743
Ramesh Bhat <rbhat1@users.sourceforge.net>
2008-10-06 11:28:00 +0000

    revised help

--- bpsr/mon/display_help.c
+++ bpsr/mon/display_help.c
@@ -4,6 +4,8 @@
 /*                                                                         */
 /* it displays the help on command line                                    */
 /*                                                                         */
+/* RB 06 Oct 2008:	revised 					   */
+/*                                                                         */
 /***************************************************************************/
 
 #include "plot4mon.h"
@@ -11,16 +13,19 @@
 void display_help(char *program) 
 {
   puts("");
-  printf("PROGRAM \"%s\" is a code included in the BPSRMON version: %.1f \n",
+  printf("PROGRAM \"%s\" is a part of BPSRMON version: %.1f ",
            program, BPSRMON_VERSION);
-  printf("         it produces various plots for monitoring BPSR activity\n");
+  printf("It produces various plots to monitor BPSR activity\n");
   puts("");
-  puts("Usage: plot4mon [filename0] [filename1] {-h, -g, ...}");
+  puts("Usage: plot4mon [filename0] [filename1] {-h, -g, -t, -l ...}");
   puts("");
-  puts("filename0   -  data file [bandpass/time series] for polarisation 0");
-  puts("filename1   -  data file [bandpass/time series] for polarisation 1");
-  puts("-h          -  print this help page");
-  puts("-log        -  plot bandpass on log scale ");
-  printf("-g <device> -  display device for plot e.g. /xs  (def=%s) \n", STD_DEVICE);
+  puts("filename0    -  data file [bandpass/time series] for polarisation 0");
+  puts("filename1    -  data file [bandpass/time series] for polarisation 1");
+  puts("-h           -  print this help page");
+  puts("-nolabel     -  omit plot labels and title ");
+  puts("-nobox       -  omit plot box ");
+  puts("-log         -  plot bandpass on log scale ");
+  printf("-g <device>  -  display device for plot e.g. /xs  (def=%s) \n", STD_DEVICE);
+  printf("-G <AAAxBBB> -  display resolution for plot e.g. -G 240x200 \n");
   puts("");
 }
