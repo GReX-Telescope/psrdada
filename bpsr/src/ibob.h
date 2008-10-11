936dbae2f92c2829e6ab1418cb2bbbab17f77abf
Willem van Straten <straten@users.sourceforge.net>
2008-10-11 06:02:57 +0000

    fully emulate telnet

--- bpsr/src/ibob.h
+++ bpsr/src/ibob.h
@@ -2,16 +2,25 @@
 #define IBOB_VLAN_BASE "169.254.128."
 #define IBOB_PORT 23
 
-static const char fake_telnet[] = { 255, 251, 37,
-				    255, 253, 38,
-				    255, 251, 38,
-				    255, 253, 3,
-				    255, 251, 24,
-				    255, 251, 31,
-				    255, 251, 32,
-				    255, 251, 33,
-				    255, 251, 34,
-				    255, 251, 39,
-				    255, 253, 5,
-				    255, 251, 35,
-				    0 };
+static const char emulate_telnet_msg1[] = { 255, 251, 37,
+				            255, 253, 38,
+				            255, 251, 38,
+				            255, 253, 3,
+				            255, 251, 24,
+				            255, 251, 31,
+				            255, 251, 32,
+				            255, 251, 33,
+				            255, 251, 34,
+				            255, 251, 39,
+				            255, 253, 5,
+				            255, 251, 35,
+				            0 };
+
+static const char emulate_telnet_resp[] = { 255, 251, 1, 
+                                            255, 251, 3, 
+                                            0 };
+
+static const char emulate_telnet_msg2[] = { 255, 253, 1, 0 };
+
+static const char ibob_prompt[] = "IBOB % ";
+
