ba79aff1e89146aa6ad4ad71dcc88953e57392a4
Willem van Straten <straten@users.sourceforge.net>
2008-10-13 19:14:30 +0000

    ibob library interface declarations

--- bpsr/src/ibob.h
+++ bpsr/src/ibob.h
@@ -1,26 +1,92 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2008 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source$
+   $Revision$
+   $Date$
+   $Author$ */
+
+#ifndef __DADA_IBOB_H
+#define __DADA_IBOB_H
+
+#include <time.h>
+#include <sys/types.h>
 
 #define IBOB_VLAN_BASE "169.254.128."
 #define IBOB_PORT 23
 
-static const char emulate_telnet_msg1[] = { 255, 251, 37,
-				            255, 253, 38,
-				            255, 251, 38,
-				            255, 253, 3,
-				            255, 251, 24,
-				            255, 251, 31,
-				            255, 251, 32,
-				            255, 251, 33,
-				            255, 251, 34,
-				            255, 251, 39,
-				            255, 253, 5,
-				            255, 251, 35,
-				            0 };
+/* ************************************************************************
 
-static const char emulate_telnet_resp[] = { 255, 251, 1, 
-                                            255, 251, 3, 
-                                            0 };
+   ibob_t - a struct and associated routines for creating and managing,
+   as well as reading and writing to and from an iBoB
 
-static const char emulate_telnet_msg2[] = { 255, 253, 1, 0 };
+   ************************************************************************ */
 
-static const char ibob_prompt[] = "IBOB % ";
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+  typedef struct {
+
+    //! Host name of iBoB interface
+    char* host;
+
+    //! Port number of iBob interface
+    int port;
+
+    //! File descriptor of open socket; -1 otherwise
+    int fd;
+
+    //! Buffer used for ibob communication
+    char* buffer;
+
+    //! Size of buffer reserved for ibob communication
+    unsigned buffer_size;
+
+    //! Emulate telnet flag must be set before call to ibob_open
+    char emulate_telnet;
+
+  } ibob_t;
+
+  /*! allocate and initialize a new ibob_t struct */
+  ibob_t* ibob_construct ();
+
+  /*! free all resources reserved for ibob communications */
+  int ibob_destroy (ibob_t* bob);
+
+  /*! set the host and port number of the ibob */
+  int ibob_set_host (ibob_t* bob, const char* host, int port);
+
+  /*! set the ibob number (use default IP base and port) */
+  int ibob_set_number (ibob_t* bob, unsigned number);
+
+  /*! open socket connection with currently configured ibob */
+  int ibob_open (ibob_t* bob);
+
+  /*! close socket connection with currently open ibob */
+  int ibob_close (ibob_t* bob);
+
+  /*! return true if already open */
+  int ibob_is_open (ibob_t* bob);
+
+  /*! reset packet counter on next UTC second, returned */
+  time_t ibob_arm (ibob_t* bob);
+
+  /*! configure the ibob */
+  int ibob_configure (ibob_t* bob, const char* mac_address);
+
+  /*! send message to ibob */
+  ssize_t ibob_send (ibob_t* bob, const char* message);
+
+  /*! receive response from ibob, up to bytes characters */
+  ssize_t ibob_recv (ibob_t* bob, char* response, size_t bytes);
+
+#ifdef __cplusplus
+	   }
+#endif
+
+#endif
