d799c9b250bbfa5b0a1d320e9682673fc9807f0b
Andrew Jameson <ajameson@users.sourceforge.net>
2008-10-14 03:10:34 +0000

    Removed files that have been specialised into apsr/bpsr sub directories and implement changes for multi instrument interopability

--- scripts/Dada.pm
+++ scripts/Dada.pm
@@ -1,5 +1,7 @@
 package Dada;
 
+use lib $ENV{"DADA_ROOT"}."/bin";
+
 use IO::Socket;     # Standard perl socket library
 use IO::Select;     # Allows select polling on a socket
 use strict;
@@ -909,7 +911,7 @@ sub killProcess($$) {
 
   (my $module, my $pname) = @_;
 
-  my $cmd = "ps axu | grep ".$pname." | grep -v grep";
+  my $cmd = "ps axu | grep \"".$pname."\" | grep -v grep";
   my $pids_to_kill = `$cmd`;
   # If the process was running
 
@@ -923,14 +925,24 @@ sub killProcess($$) {
     for ($i=0;$i<=$#arr;$i++) {
       my $pid = @arr[$i];
       $cmd = "kill -TERM ".$pid;
-      print "Dada->killProcess() ".$cmd."\n";
       system($cmd);
     }
 
     if ($? == 0) {
       return ("ok", "process killed");
     } else {
-      return ("fail", "process not killed");
+
+      # Now try with a KILL signal
+      for ($i=0;$i<=$#arr;$i++) {
+        my $pid = @arr[$i];
+        $cmd = "kill -KILL".$pid;
+        system($cmd);
+      }
+      if ($? == 0) {
+        return ("ok", "required -KILL signal");
+      } else {
+        return ("fail", "process not killed");
+      }
     }
 
   } else {
@@ -988,65 +1000,6 @@ sub readCFGFileIntoHash($$$) {
     }
   }
   return %values;
-}
-
-sub makePlotsFromArchives($$$$$$) {
-                      
-  my ($module, $plotDir, $dir, $total_f_res, $total_t_res, $resolution) = @_;
-
-  use constant PHASE_VS_TIME_FILE  => "phase_vs_time";
-  use constant PHASE_VS_FREQ_FILE  => "phase_vs_freq";
-  use constant PHASE_VS_FLUX_FILE  => "phase_vs_flux";
-  use constant IMAGE_TYPE          => ".png";
-
-
-  my %local_cfg = getDadaConfig();
-  my $web_style_txt = $local_cfg{"SCRIPTS_DIR"}."/web_style.txt";
-
-  my $psrplot_args = "-g ".$resolution." -jp";
-
-  # If we are plotting hi-res - include 
-  if ($resolution eq "1024x768") {
-    # No changes
-  } else {
-    $psrplot_args .= " -s ".$web_style_txt." -c below:l=unset";
-  }
-
-  my $bindir = getCurrentBinaryVersion();
-  my $timestamp = getCurrentDadaTime("Dada",0);
-  my $phase_vs_time = $dir."/".PHASE_VS_TIME_FILE."_".$timestamp."_".$resolution.IMAGE_TYPE;
-  my $phase_vs_freq = $dir."/".PHASE_VS_FREQ_FILE."_".$timestamp."_".$resolution.IMAGE_TYPE;
-  my $phase_vs_flux = $dir."/".PHASE_VS_FLUX_FILE."_".$timestamp."_".$resolution.IMAGE_TYPE;
-
-  my $cmd = "rm -f ".$dir."/*_".$resolution.IMAGE_TYPE;
-  `$cmd`;
-
-  # Combine the archives from the machine into the archive to be processed
-  # PHASE vs TIME
-  $cmd = $bindir."/psrplot ".$psrplot_args." -p time -jF -D ".$phase_vs_time."/png ".$total_t_res;
-  # print $cmd."\n";
-  `$cmd`;
-
-  # PHASE vs FREQ
-  $cmd = $bindir."/psrplot ".$psrplot_args." -p freq -jT -D ".$phase_vs_freq."/png ".$total_f_res;
-  # print $cmd."\n";
-  `$cmd`;
-
-  # PHASE vs TOTAL INTENSITY
-  $cmd = $bindir."/psrplot ".$psrplot_args." -p flux -jTF -D ".$phase_vs_flux."/png ".$total_f_res;
-  # print $cmd."\n";
-  `$cmd`;
-
-  # If we want to make these plots the "current ones"
-  if ($plotDir ne "") {
-
-    $cmd = "rm -f ".$plotDir."/*_".$resolution.IMAGE_TYPE;
-    `$cmd`;
-
-    $cmd = "cp ".$phase_vs_time." ".$phase_vs_freq." ".$phase_vs_flux." ".$plotDir."/";
-    `$cmd`;
-  }
-
 }
 
 sub daemonize($$$) {
