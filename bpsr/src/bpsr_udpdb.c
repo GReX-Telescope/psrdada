266e3486423398ca239fdbe37dfe2aa5b166da78
Andrew Jameson <ajameson@users.sourceforge.net>
2008-10-14 03:30:22 +0000

    Added shared memory block, PWC control port and multilog log command line options

--- bpsr/src/bpsr_udpdb.c
+++ bpsr/src/bpsr_udpdb.c
@@ -20,15 +20,18 @@ void usage()
 	   "bpsr_udpdb [options]\n"
      " -h             print help text\n"
      " -i interface   ip/interface for inc. UDP packets [default all]\n"
-	   " -p             port on which to listen [default %d]\n"
+	   " -p port        port on which to listen [default %d]\n"
 	   " -d             run as daemon\n"
+	   " -k             hexidecimal shared memor key [default %x]\n"
      " -m mode        1 for independent mode, 2 for controlled mode\n"
      " -v             verbose messages\n"
      " -1             one time only mode, exit after EOD is written\n"
      " -H filename    ascii header information in file\n"
-     " -c             don't verify udp headers against header block\n"
-     " -S num         file size in bytes\n", 
-     BPSR_DEFAULT_UDPDB_PORT);
+     " -S num         file size in bytes\n"
+     " -l port        multilog port to write logging output to [default %d]\n"
+     " -c port        port to received pwcc commands on [default %d]\n",
+     BPSR_DEFAULT_UDPDB_PORT, DADA_DEFAULT_BLOCK_KEY,
+     BPSR_DEFAULT_PWC_LOGPORT, BPSR_DEFAULT_PWC_PORT);
 }
 
 
@@ -563,6 +566,12 @@ int main (int argc, char **argv)
   /* port on which to listen for udp packets */
   int port = BPSR_DEFAULT_UDPDB_PORT;
 
+  /* pwcc command port */
+  int c_port = BPSR_DEFAULT_PWC_PORT;
+
+  /* multilog output port */
+  int l_port = BPSR_DEFAULT_PWC_LOGPORT;
+
   /* Flag set in daemon mode */
   char daemon = 0;
 
@@ -583,6 +592,10 @@ int main (int argc, char **argv)
   /* size of the header buffer */
   uint64_t header_size = 0;
 
+  /* hexadecimal shared memory key */
+  key_t dada_key = DADA_DEFAULT_BLOCK_KEY;
+
+
   /* actual struct with info */
   udpdb_t udpdb;
 
@@ -594,11 +607,15 @@ int main (int argc, char **argv)
   static char* buffer = 0;
   char *src;
 
-  //char * ibob_host;
-  //int    ibob_port;
-
-  while ((arg=getopt(argc,argv,"di:p:vm:S:H:n:1h")) != -1) {
+  while ((arg=getopt(argc,argv,"k:di:p:vm:S:H:n:1hc:l:")) != -1) {
     switch (arg) {
+
+    case 'k':
+      if (sscanf (optarg, "%x", &dada_key) != 1) {
+        fprintf (stderr,"bpsr_udpdb: could not parse key from %s\n",optarg);
+        return -1;
+      }
+      break;
       
     case 'd':
       daemon = 1;
@@ -651,6 +668,24 @@ int main (int argc, char **argv)
       }
       break;
 
+    case 'c':
+      if (optarg) {
+        c_port = atoi(optarg);
+        break;
+      } else {
+        usage();
+        return EXIT_FAILURE;
+      }
+
+    case 'l':
+      if (optarg) {
+        l_port = atoi(optarg);
+        break;
+      } else {
+        usage();
+        return EXIT_FAILURE;
+      }
+
     case 'h':
       usage();
       return 0;
@@ -669,7 +704,7 @@ int main (int argc, char **argv)
   else
     multilog_add (log, stderr);
 
-  multilog_serve (log, DADA_DEFAULT_PWC_LOG);
+  multilog_serve (log, l_port);
   
   if (verbose) 
     fprintf (stderr, "Creating dada pwc main\n");
@@ -709,11 +744,13 @@ int main (int argc, char **argv)
   udpdb.prev_time = time(0);
   udpdb.current_time = udpdb.prev_time;
   udpdb.error_seconds = 10;
-  udpdb.statslog = multilog_open ("bpsr_udpdb_stats", 0);
-  multilog_serve (udpdb.statslog, BPSR_DEFAULT_UDPDB_STATS);
+  //udpdb.statslog = multilog_open ("bpsr_udpdb_stats", 0);
+  //multilog_serve (udpdb.statslog, BPSR_DEFAULT_UDPDB_STATS);
     
   /* Connect to shared memory */
   hdu = dada_hdu_create (pwcm->log);
+
+  dada_hdu_set_key(hdu, dada_key);
 
   if (dada_hdu_connect (hdu) < 0)
     return EXIT_FAILURE;
@@ -821,6 +858,8 @@ int main (int argc, char **argv)
 
     if (verbose) fprintf (stderr, "Creating dada pwc control interface\n");
     pwcm->pwc = dada_pwc_create();
+
+    pwcm->pwc->port = c_port;
 
     if (verbose) fprintf (stderr, "Creating dada server\n");
     if (dada_pwc_serve (pwcm->pwc) < 0) {
