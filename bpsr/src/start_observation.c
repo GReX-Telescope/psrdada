c0039cfb44df54702046ec86ab0481aca023474b
Willem van Straten <straten@users.sourceforge.net>
2008-10-16 00:10:56 +0000

    shell of routine to coordinate starting on a known UTC tick

---
+++ bpsr/src/start_observation.c
@@ -0,0 +1,21 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2008 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include <sys/time.h>
+#include <time.h>
+
+/*! Starts an observation on the next UTC second, returned as time_t
+
+    \param start_routine the function that does the starting
+    \param arguments the arguments passed to start_routine
+*/
+
+time_t start_observation( int(*start_routine)(void*), void* arg )
+{
+  start_routine (arg);
+  return 0;
+}
