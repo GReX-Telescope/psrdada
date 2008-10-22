480d80d6cc3066c7d9270d2c052f7b86971a68f7
Willem van Straten <straten@users.sourceforge.net>
2008-10-22 11:55:18 +0000

    option to read next header block as a passive viewer would

--- src/dada_header.c
+++ src/dada_header.c
@@ -22,9 +22,10 @@
 
 void usage()
 {
-fprintf (stdout,
+  fprintf (stdout,
      "dada_header [options]\n"
      " -k         hexadecimal shared memory key  [default: %x]\n"
+     " -p         read header block as a passive viewer \n"
      " -v         be verbose\n",DADA_DEFAULT_BLOCK_KEY);
 }
 
@@ -40,6 +41,9 @@ int main (int argc, char **argv)
   /* Flag set in verbose mode */
   char verbose = 0;
 
+  /* Open as a passive viewer */
+  char passive = 0;
+
  /* dada key for SHM */
   key_t dada_key = DADA_DEFAULT_BLOCK_KEY;
 
@@ -48,15 +52,20 @@ int main (int argc, char **argv)
   /* TODO the amount to conduct a busy sleep inbetween clearing each sub
    * block */
 
-  while ((arg=getopt(argc,argv,"vk:")) != -1)
-    switch (arg) {
+  while ((arg=getopt(argc,argv,"pvk:")) != -1)
+    switch (arg)
+    {
+    case 'p':
+      passive = 1;
+      break;
 
     case 'v':
       verbose=1;
       break;
 
     case 'k':
-      if (sscanf (optarg, "%x", &dada_key) != 1) {
+      if (sscanf (optarg, "%x", &dada_key) != 1)
+      {
         fprintf (stderr, "dada_header: could not parse key from %s\n", optarg);
         return -1;
       }
@@ -65,7 +74,6 @@ int main (int argc, char **argv)
     default:
       usage ();
       return 0;
-
     }
 
   log = multilog_open ("dada_header", 0);
@@ -78,14 +86,22 @@ int main (int argc, char **argv)
   if (verbose) 
     fprintf(stderr,"Connecting to header block\n");
 
-  if (dada_hdu_connect (hdu) < 0) {
+  if (dada_hdu_connect (hdu) < 0)
+  {
     fprintf(stderr, "Could not connect to the header and data blocks\n");
     return EXIT_FAILURE;
   }
 
-  if (dada_hdu_lock_read(hdu) < 0) {
-   fprintf(stderr, "Could not lock Header Block for reading\n");
-   return EXIT_FAILURE;
+  if (!passive && dada_hdu_lock_read(hdu) < 0)
+  {
+    fprintf(stderr, "Could not lock Header Block for reading\n");
+    return EXIT_FAILURE;
+  }
+
+  if (passive && dada_hdu_open_view(hdu) < 0)
+  {
+    fprintf(stderr, "Could not lock Header Block for viewing\n");
+    return EXIT_FAILURE;
   }
 
 
@@ -97,26 +113,29 @@ int main (int argc, char **argv)
 
   /* Wait for the next valid header sub-block */
   header = ipcbuf_get_next_readable (hdu->header_block, &header_size);
-  if (!header) {
+  if (!header)
+  {
     fprintf(stderr,"Could not get next header\n");
     return DADA_ERROR_FATAL;
   }
 
   header_size = ipcbuf_get_bufsz (hdu->header_block);
 
-  if (verbose) {
+  if (verbose)
+  {
     fprintf(stderr,"HEADER BEGIN\n");
     fprintf(stderr,"======================================================\n");
   }
 
   fprintf(stdout,"%s",header);
 
-  if (verbose) {
+  if (verbose)
+  {
     fprintf(stderr,"======================================================\n");
     fprintf(stderr,"HEADER END\n");
   }
 
-  if (dada_hdu_unlock_read (hdu) < 0)
+  if (!passive && dada_hdu_unlock_read (hdu) < 0)
     return EXIT_FAILURE;
   
   if (dada_hdu_disconnect (hdu) < 0)
@@ -124,3 +143,4 @@ int main (int argc, char **argv)
 
   return EXIT_SUCCESS;
 }
+
