058002c05d11d43457095eb9b71934d95b139130
Willem van Straten <straten@users.sourceforge.net>
2008-11-18 05:21:22 +0000

    Willem and Andrea: added plot options; merged with AJ's unchecked-in code

--- bpsr/scripts/client_bpsr_results_monitor.pl
+++ bpsr/scripts/client_bpsr_results_monitor.pl
@@ -94,22 +94,56 @@ my $dir;
 
 my $daemon_control_thread = threads->new(\&daemonControlThread);
 
-my @lines;
-my $line;
-my $find_result;
 my $result;
 my $response;
+
+my $mon_files_processed = 0;
+my $dspsr_files_processed = 0;
 
 # Loop until daemon control thread asks us to quit
 while (!($quit_daemon)) {
 
-  @lines = ();
+  $mon_files_processed = process_mon_files();
 
-  #TODO new code
-  $cmd = "find . -maxdepth 3 -regex \".*[ts|bp|bps][0|1]\" | sort";
-  $find_result = `$cmd`;
+  $dspsr_files_processed = process_dspsr_files();
 
-  @lines = split(/\n/,$find_result);
+  # If nothing is happening, have a snooze
+  if (($mon_files_processed + $dspsr_files_processed) < 1) {
+    sleep(SLEEPTIME);
+  }
+
+}
+
+# Rejoin our daemon control thread
+$daemon_control_thread->join();
+
+logMessage(0, "INFO", "STOPPING SCRIPT");
+
+# Close the nexus logging connection
+Dada->nexusLogClose($log_socket);
+
+
+exit (0);
+
+
+###############################################################################################
+##
+## Functions
+##
+
+#
+# Look for mon files produced by the_decimator
+#
+sub process_mon_files() {
+
+  my $cmd = "find . -maxdepth 3 -regex \".*[ts|bp|bps][0|1]\" | sort";
+  my $find_result = `$cmd`;
+
+  my @lines = split(/\n/,$find_result);
+  my $line = "";
+
+  my $result = "";
+  my $response = "";
 
   foreach $line (@lines) {
 
@@ -130,7 +164,6 @@ while (!($quit_daemon)) {
     } else {
 
       my $aux_dir = $file_dir."/aux";
-
       my $aux_tar = $file_dir."/aux.tar";
 
       if (! -d $aux_dir) {
@@ -150,42 +183,140 @@ while (!($quit_daemon)) {
         # add the mon file to the .tar archive
         chdir $file_dir ;
 
-        if (! -f $aux_tar) {
-          $cmd = "tar -cf ./".$aux_tar." aux/".$file;
+        if (! -f "./aux.tar") {
+          $cmd = "tar -cf ./aux.tar aux/".$file;
         } else {
-          $cmd = "tar -rf ./".$aux_tar." aux/".$file;
+          $cmd = "tar -rf ./aux.tar aux/".$file;
         }
 
-        ($result, $response) = Dada->mySystem($cmd,0);
-        if ($result ne "ok") {
-          logMessage(0, "ERROR", "Could not add file ($file) to aux.tar \"".$response."\"");
-        }
+         ($result, $response) = Dada->mySystem($cmd,0);
+         if ($result ne "ok") {
+           logMessage(0, "ERROR", "Could not add file ($file) to aux.tar \"".$response."\"");
+         }
 
-        chdir $client_archive_dir;
+         chdir $cfg{"CLIENT_ARCHIVE_DIR"};
       }
     }
   }
 
-  if ($#lines == -1) {
-    sleep(SLEEPTIME);
+  # return the number
+  return ($#lines + 1);
+}
+
+#
+# process any dspsr output files produced
+#
+sub process_dspsr_files() {
+
+  my $cmd = "find . -maxdepth 3 -name \"20*\.ar\" | sort";
+  my $find_result = `$cmd`;
+
+  my @lines = split(/\n/,$find_result);
+  my $line = "";
+
+  my $result = "";
+  my $response = "";
+
+  my $bindir = Dada->getCurrentBinaryVersion();
+
+  foreach $line (@lines) {
+
+    $line = substr($line,2);
+
+    logMessage(2, "INFO", "Processing dspsr file \"".$line."\"");
+
+    my ($utc, $beam, $file) = split( /\//, $line);
+                                                                                                                    
+    my $file_dir = $utc."/".$beam;
+
+    my $sumd_archive = $file_dir."/integrated.ar";
+    my $curr_archive = $file_dir."/".$file;
+    my $temp_archive = $file_dir."/temp.ar";
+
+    if (! -f $sumd_archive) {
+      $cmd = "cp ".$curr_archive." " .$temp_archive;
+    } else {
+      $cmd = $bindir."/psradd -s -f ".$temp_archive." ".$sumd_archive." ".$curr_archive;
+    }    
+
+    logMessage(1, "INFO", $cmd); 
+    ($result, $response) = Dada->mySystem($cmd);
+    if ($result ne "ok") {
+
+      logMessage(0, "ERROR", "process_dspsr_files: ".$cmd." failed: ".$response);
+      unlink($temp_archive);
+
+    } else {
+
+      logMessage(2, "INFO", "process_dspsr_files: archive added");
+
+      logMessage(2, "INFO", "unlink(".$sumd_archive.")");
+      unlink($sumd_archive);
+      logMessage(2, "INFO", "rename(".$temp_archive.", ".$sumd_archive.")");
+      rename($temp_archive, $sumd_archive);
+
+      my $plot_file = "";
+      my $plot_binary = $bindir."/psrplot";
+      my $time_str = Dada->getCurrentDadaTime();
+
+      $plot_file = makePhaseVsFreqPlotFromArchive($plot_binary, $time_str, "112x84", $sumd_archive, $file_dir);
+      if ($plot_file ne "none") {
+         ($result, $response) = sendToServerViaNFS($file_dir."/".$plot_file, $cfg{"SERVER_RESULTS_NFS_MNT"}, $file_dir);        
+         unlink($plot_file);
+      }
+
+      $plot_file = makePhaseVsFreqPlotFromArchive($plot_binary, $time_str, "400x300", $sumd_archive, $file_dir);
+      if ($plot_file ne "none") {
+         ($result, $response) = sendToServerViaNFS($file_dir."/".$plot_file, $cfg{"SERVER_RESULTS_NFS_MNT"}, $file_dir);
+         unlink($plot_file);
+      }
+
+      $plot_file = makePhaseVsFreqPlotFromArchive($plot_binary, $time_str, "1024x768", $sumd_archive, $file_dir);
+      if ($plot_file ne "none") {
+         ($result, $response) = sendToServerViaNFS($file_dir."/".$plot_file, $cfg{"SERVER_RESULTS_NFS_MNT"}, $file_dir);
+         unlink($plot_file);
+      }
+    }
+
+    unlink ($curr_archive);
+
   }
+
+  return ($#lines+1);
 
 }
 
-# Rejoin our daemon control thread
-$daemon_control_thread->join();
-
-logMessage(0, "INFO", "STOPPING SCRIPT");
-
-# Close the nexus logging connection
-Dada->nexusLogClose($log_socket);
-
-
-exit (0);
 
 #
-# Functions
+# create a phase vs freq plot from a dspsr archive
 #
+sub makePhaseVsFreqPlotFromArchive($$$$$) {
+
+  my ($binary, $timestr, $res, $archive, $dir) = @_;
+
+  # remove axes, labels and outside spaces for lil'plots
+  my $add = "";
+  if ($res eq "112x84") {
+    $add = " -s ".$cfg{"SCRIPTS_DIR"}."/web_style.txt -c below:l=unset";
+  }
+
+  my $args = "-g ".$res.$add." -jp -p freq -jT -j\"zap median,F 256,D\"";
+  my $plotfile = $timestr.".pvf_".$res.".png";
+
+  my $cmd = $binary." ".$args." -D ".$dir."/".$plotfile."/png ".$archive;
+
+  logMessage(2, "INFO", $cmd);
+  ($result, $response) = Dada->mySystem($cmd);
+
+  if ($result ne "ok") {
+    logMessage(0, "ERROR", "plot cmd \"".$cmd."\" failed: ".$response);
+    $plotfile = "none";
+  }
+
+  return $plotfile;
+
+}
+
 
 sub sendToServerViaNFS($$$) {
 
@@ -205,7 +336,7 @@ sub sendToServerViaNFS($$$) {
   if ($result ne "ok") {
     return ("fail", "Command was \"".$cmd."\" and response was \"".$response."\"");
   } else {
-    return "ok";
+    return ("ok", "");
   }
 
 }
