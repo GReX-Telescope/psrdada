3f0888e4f222ec472b2bcf9e77aaa19eaed9a1e8
Willem van Straten <straten@users.sourceforge.net>
2008-11-19 10:45:44 +0000

    fixed potential segfault in corrupted filename (strcat instead of strcpy)

--- bpsr/mon/create_outputname.c
+++ bpsr/mon/create_outputname.c
@@ -13,66 +13,46 @@
 void create_outputname( char inpfile[], char inpdev[], char outputfile[],
 			int plotnum, char add_work[])
 {
-  int dot_position;
-  char extens[80];
-  char *pn2extens;
-  char newfile[80];
-  char newinp[80];
-  char ext[10];
+  char *found = 0;
+  char ext[8];
 
-  if (plotnum==0)
+  if (!( (strings_compare(inpdev,"/xs")) || (strings_compare(inpdev,"/XS")) ||
+	 (strings_compare(inpdev,"/xw")) || (strings_compare(inpdev,"/XW")) ))
+  {
+    if (plotnum==0)
     {
-      if (!( (strings_compare(inpdev,"/xs")) || (strings_compare(inpdev,"/XS"))
-        ||(strings_compare(inpdev,"/xw")) || (strings_compare(inpdev,"/XW")) ))
-       {
-         if (strstr(inpfile, "bps") != NULL) strcpy(ext,".bps");
-         else if (strstr(inpfile, "bp") != NULL) strcpy(ext,".bp");
-         else if (strstr(inpfile, "ts") != NULL) strcpy(ext,".ts");
-         else strcpy(ext,"");
-
-         strcpy(newinp,inpfile);
-         dot_position=strcspn(inpfile,".");
-         newinp[dot_position]='\0';
-         strcpy(newfile,newinp);
-	 strcpy(extens,inpdev);
-	 while((pn2extens=strpbrk(extens,"/"))!=NULL) *pn2extens='.';
-	 strcat(newfile,ext);
-	 strcat(newfile,extens);
-	 printf(" The output file will be %s \n",newfile);
-	 strcat(newfile,inpdev);
-	 strcat(outputfile,newfile);
-       }
-      else
-       {
-	 strcpy(outputfile,inpdev);
-	printf(" The output file will be directed to %s screen \n",outputfile);
-       }
-
+      if (strstr(inpfile, "bps") != NULL) strcpy(ext,".bps");
+      else if (strstr(inpfile, "bp") != NULL) strcpy(ext,".bp");
+      else if (strstr(inpfile, "ts") != NULL) strcpy(ext,".ts");
+      else strcpy(ext,"");
     }
-  else if (plotnum==1) 
+
+    strcpy (outputfile,inpfile);
+
+    found = strchr (outputfile,'.');
+    if (found)
+      *found = '\0';
+
+    if (plotnum == 0)
+      strcat (outputfile, ext);
+    else
     {
-      if (!( (strings_compare(inpdev,"/xs")) || (strings_compare(inpdev,"/XS"))
-        ||(strings_compare(inpdev,"/xw")) || (strings_compare(inpdev,"/XW")) ))
-       {
-         strcpy(newinp,inpfile);
-         dot_position=strcspn(inpfile,".");
-         newinp[dot_position]='\0';
-	 strcpy(newfile,newinp);
-	 strcat(newfile,".");
-	 strcat(newfile,add_work);
-	 strcpy(extens,inpdev);
-	 while((pn2extens=strpbrk(extens,"/"))!=NULL) *pn2extens='.';
-	 strcat(newfile,extens);
-	 printf(" The output file will be %s \n",newfile);
-	 strcat(newfile,inpdev);
-	 strcpy(outputfile,newfile);
-       }
-      else
-       {
-	 strcpy(newfile,"2");
-	 strcat(newfile,inpdev);
-	 strcpy(outputfile,newfile);
-	printf(" The output file will be directed to %s screen \n",outputfile);
-       }
+      strcat(outputfile,".");
+      strcat(outputfile,add_work);
     }
+
+    strcpy(ext,inpdev);
+
+    while((found=strpbrk(ext,"/"))!=NULL) *found='.';
+
+    strcat(outputfile,ext);
+    strcat(outputfile,inpdev);
+  }
+  else
+  {
+    outputfile[0] = '1' + plotnum;
+    outputfile[1] = '\0';
+    strcat(outputfile,inpdev);
+    printf(" The output file will be directed to %s screen \n",outputfile);
+  }
 }
