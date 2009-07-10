147fc83f47beb4bc4ee93290628126cdf8a67171
Andrew Jameson <ajameson@users.sourceforge.net>
2009-07-10 05:12:34 +0000

    Support added for command line meta file

--- src/slow_rm.c
+++ src/slow_rm.c
@@ -25,11 +25,11 @@ int main (int argc, char **argv)
   /* list of file names to delete */
   char ** f_names = 0;
 
-  /* file sizes of file names */
-  uint64_t *f_sizes = 0;
+  /* file size of currently open file */
+  uint64_t f_size = 0;
 
-  /* file descriptors of files */
-  int *fds = 0;
+  /* file descriptor for deleting */
+  int fd = 0;
 
   /* deletion rate in MB/s */
   unsigned rate = DEFAULT_DELETION_RATE;
@@ -94,23 +94,20 @@ int main (int argc, char **argv)
     if (verbose)
       fprintf(stdout, "%d files provided on the command line\n", n_files);
 
-    fds = (int *) malloc(sizeof(int) * n_files);
     f_names = (char **) malloc(sizeof(char *) * n_files);
-    f_sizes = (uint64_t *) malloc(sizeof(uint64_t) * n_files);
-
-    if ((!fds) || (!f_names) || (!f_sizes)) {
+    if (!f_names) {
       fprintf(stderr, "Could not malloc memory\n");
       return(EXIT_FAILURE);
     }
 
     for (file_i=optind; file_i<argc; file_i++, i++)
       f_names[i] = strdup(argv[file_i]);
-  }
-  else {
+
+  } else {
 
     FILE* fptr = fopen (metafile, "r");
     if (!fptr) {
-      fprintf (stderr, "Could not open '%s' for reading: %s\n", f_names[i], strerror(errno));
+      fprintf (stderr, "Could not open '%s' for reading: %s\n", metafile, strerror(errno));
       return(EXIT_FAILURE);
     }
 
@@ -119,8 +116,8 @@ int main (int argc, char **argv)
       n_files ++;
       f_names = (char **) realloc (f_names, sizeof(char *) * n_files);
       if (!f_names) {
-	fprintf(stderr, "Could not realloc memory\n");
-	return(EXIT_FAILURE);
+        fprintf(stderr, "Could not realloc memory\n");
+        return(EXIT_FAILURE);
       }
       char* newline = strchr (name, '\n');
       if (newline) *newline = '\0';
@@ -128,133 +125,115 @@ int main (int argc, char **argv)
       f_names[i] = strdup(name);
       i++;
     }
-
-    fds = (int *) malloc(sizeof(int) * n_files);
-    f_sizes = (uint64_t *) malloc(sizeof(uint64_t) * n_files);
-    if ((!fds) || (!f_sizes)) {
-      fprintf(stderr, "Could not malloc memory\n");
-      return(EXIT_FAILURE);
-    }
+    if (verbose)
+      fprintf(stdout, "%d files provided in Meta File\n", n_files);
 
   }
 
-  unsigned file_start = 0;
-  unsigned file_end = 0;
 
-  while (file_start < n_files)
+  if (verbose) 
+    fprintf(stdout, "Truncating %d files at %d MB/s\n", n_files, rate);
+
+  for (i=0; i < n_files; i++)
   {
-    for (i=file_start; i < n_files; i++)
-    {
-      /* try to open the file for writing */
-      fds[i] = open(f_names[i], O_WRONLY);
-      if (fds[i] < 0) {
+    f_size = -1;
 
-	if (errno == EMFILE)
-	  break;
-
-	fprintf(stderr, "Failed to open '%s' for writing: %s\n", f_names[i], strerror(errno));
-	return(EXIT_FAILURE);
-      }
+    /* try to open the file for writing */
+    fd = open(f_names[i], O_WRONLY);
+    if (fd < 0) {
+      fprintf(stderr, "Failed to open '%s' for writing: %s\n", f_names[i], strerror(errno));
+      //return(EXIT_FAILURE);
+      //
+    } else {
 
       /* stat the file to check we can delete it */
-      status = fstat(fds[i], &filestat);
+      status = fstat(fd, &filestat);
       if (status != 0) {
-	fprintf(stderr, "Failed to stat '%s': %s\n", f_names[i], strerror(errno));
-	return(EXIT_FAILURE);
-      }
+        fprintf(stderr, "Failed to stat '%s': %s\n", f_names[i], strerror(errno));
+        //return(EXIT_FAILURE);
+      } else {
 
-      if (S_ISDIR(filestat.st_mode)) {
-	printf("Cannot handle %s (directory)\n", argv[file_i]);
-	return(EXIT_FAILURE);
-      }
+        if (S_ISDIR(filestat.st_mode)) {
+          printf("Cannot handle %s (directory)\n", argv[file_i]);
+          //return(EXIT_FAILURE);
+        } else {
 
-      f_sizes[i] = (uint64_t) filestat.st_size;
-      if (verbose) {
-	fprintf(stdout, "%s: %"PRIu64" bytes\n", f_names[i], f_sizes[i]);
-      }
+          f_size = (uint64_t) filestat.st_size;
+          if (verbose)
+            fprintf(stdout, "%s: %"PRIu64" bytes\n", f_names[i], f_size);
+  
+          int64_t new_length = 0;
+          unsigned n_ops = 0;
+          unsigned i_op = 0;
+          int j = 0;
 
-      file_end ++;
-    }
+          if (verbose) 
+            fprintf(stderr, "%s [", f_names[i]);
 
-    int64_t new_length = 0;
-    unsigned n_ops = 0;
-    unsigned i_op = 0;
-    int j = 0;
+          i_op = 0;
+          n_ops = f_size / truncate_chunk;
+          if (n_ops == 0) 
+            n_ops = 1;
 
-    if (verbose) 
-      fprintf(stdout, "Truncating %d files at %d MB/s\n", file_end-file_start, rate);
+          if (verbose) {
+            for (j=0; j<n_ops; j++) {
+              fprintf(stderr, " ");  
+            }
+            fprintf(stderr, "]");
+          }
 
-  /* Now begin truncating each file */
-    for (i=file_start; i<file_end; i++) {
+          while (f_size > 0) {
 
-      if (verbose) 
-	fprintf(stderr, "%s [", f_names[i]);
+            new_length = f_size - truncate_chunk; 
+            if (new_length < 0)
+              new_length = 0;
 
-    i_op = 0;
-    n_ops = f_sizes[i] / truncate_chunk;
-    if (n_ops == 0) 
-      n_ops = 1;
-    if (verbose) {
-      for (j=0; j<n_ops; j++) {
-        fprintf(stderr, " ");  
-      }
-      fprintf(stderr, "]");
-    }
+            //if (verbose) {
+            //  fprintf(stdout, "truncating %s from %"PRIu64" to %"PRIu64"\n",
+            //                  f_names[i], f_size, new_length);
+            //}
 
-    while (f_sizes[i] > 0) { 
+            status = ftruncate(fd, new_length);
 
-      new_length = f_sizes[i] - truncate_chunk; 
-      if (new_length < 0)
-        new_length = 0;
-      //if (verbose) {
-      //  fprintf(stdout, "truncating %s from %"PRIu64" to %"PRIu64"\n",
-      //                  f_names[i], f_sizes[i], new_length);
-      //}
+            if (status < 0) {
+              fprintf(stderr, "truncating failed\n");
+            }
 
-      status = ftruncate(fds[i], new_length);
-      if (status < 0) {
-        fprintf(stderr, "truncating failed\n");
-      }
+            f_size = new_length;
 
-      f_sizes[i] = new_length;
+            i_op++;
 
-      i_op++;
+            if (verbose) {
+              fprintf(stderr, "\r%s [", f_names[i]);
+              for (j=0; j<n_ops; j++) {
+                if (j < i_op)
+                  fprintf(stderr, ".");
+                else
+                  fprintf(stderr, " ");
+              }
+              fprintf(stderr, "]");
+            }
 
-      if (verbose) {
-        fprintf(stderr, "\r%s [", f_names[i]);
-        for (j=0; j<n_ops; j++) {
-          if (j < i_op)
-            fprintf(stderr, ".");
-          else
-            fprintf(stderr, " ");
+            if (f_size > 0) 
+              sleep(1);
+          }
+          if (verbose)
+            fprintf(stderr, "\n");
         }
-        fprintf(stderr, "]");
       }
-
-      if (f_sizes[i] > 0) 
-        sleep(1);
     }
-    if (verbose)
-      fprintf(stderr, "\n");
-  }
 
-  /* close all the files */
-  for (i=file_start; i<file_end; i++) {
-    if (fds[i])
-      close(fds[i]);
-  }
+   /* close the file */
+    if (fd)
+      close(fd);
 
-  for (i=file_start; i<file_end; i++) {
-    if (f_sizes[i] == 0) {
+    if (f_size == 0) {
       status = unlink(f_names[i]);
-      if (status < 0) 
+      if (status < 0)
         fprintf(stderr, "could not delete truncated file: %s\n", f_names[i]);
-    } else {
+    } else
       fprintf(stderr, "file %s was not truncated correctly\n", f_names[i]);
-    }
-  }
 
-  file_start = file_end;
   }
 
   return EXIT_SUCCESS;
