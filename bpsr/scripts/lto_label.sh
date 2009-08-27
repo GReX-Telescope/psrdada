18d4469b355f83e2454bca1899c1fe1c9315a6be
Willem van Straten <straten@users.sourceforge.net>
2009-08-27 01:45:56 +0000

    labels tape and updates test database

--- bpsr/scripts/lto_label.sh
+++ bpsr/scripts/lto_label.sh
@@ -2,7 +2,7 @@
 
 device="/dev/nst1"
 project="P630"
-prefix="HITRUN"
+prefix="HTRU"
 
 usage()
 {
@@ -68,26 +68,66 @@ echo Tape device: $device
 echo Project ID: $project
 echo Label prefix: $prefix
 
-number=$1
-name=`echo $number | awk '{printf("HRE%03dS4",$1);}'`
+dbase=$HOME/bookkeeping/tape_archiver/tapes.${project}.testing
+
+echo Database: $dbase
+
+if [ ! -e $dbase ]; then
+  touch $dbase
+fi
+
+last=`grep -F $prefix $dbase | awk '{print $1}' | sort | tail -1`
+
+if test x"$last" = x; then
+  number=0
+else
+  number=`echo $last | sed -e "s/$prefix//"`
+  number=`expr $number + 1`
+fi
+
+name=`echo $number | awk '{printf("'$prefix'%04d",$1);}'`
+
+echo New tape: $name
+
+echo "Labeling tape in $device with label $name"
+echo "Press return to continue"
+read ret
+
 mkdir -p /tmp/tapelableotron
 cd /tmp/tapelableotron
 touch $name
-mt -f /dev/nst0 status
+
+mt -f $device status > /dev/null
 if [ $? -ne 0 ]; then
 echo "Tape status check failed!"
 exit
 fi
-mt -f /dev/nst0 rewind
-echo "Labeling tape in /dev/nst0 with label $name"
-echo "Press return to continue"
-read ret
+
+mt -f $device rewind
+
+mt -f $device fsf 1 2> /dev/null
+if [ $? == 0 ]; then
+echo "Tape already contains data ... aborting"
+exit
+fi
+
 echo "Labeling tape"
-tar -cf /dev/nst0 $name
+tar -cf $device $name
+
 echo "Ejecting tape"
-mt -f /dev/nst0 rewoffl
+mt -f $device rewoffl
+
 rm $name
 cd ..
-rmdir tapelableotron
-echo "Done"
+rm -rf tapelableotron
+
+echo Updating $database
+echo "$name  750.00    0.00  750.00  000001  0    0" >> $dbase
+
+echo
+echo "Please label the tape with $name"
+echo "then press return to continue"
+read ret
+
 exit
+
