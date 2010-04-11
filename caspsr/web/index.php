abd7e153b2fac08e3f920e16034285bf76dc695b
Willem van Straten <straten@users.sourceforge.net>
2010-04-11 06:09:03 +0000

    CASPSR web interface added

--- caspsr/web/index.php
+++ caspsr/web/index.php
@@ -2,27 +2,23 @@
 
 include("caspsr.lib.php");
 
+$inst = new caspsr();
 
 echo "<html>\n";
-casper::print_header("CASPSR : Casper Parkes Swinburne Recorder");
-echo "<body>\n";
-echo "hello\n";
-echo "</body>\n";
-echo "</html>\n";
+# $inst->print_head("CASPSR : Casper Parkes Swinburne Recorder");
 ?>
-<!--
 <FRAMESET ROWS="1" COLS="800px,*" frameborder=1>
-  <FRAMESET COLS=1 ROWS="60px,95px,40px,110px,250px,*" border=0>
-    <FRAME name="banner" src="statebanner.php" frameborder=0 marginheight=0 marginwidth=0>
-    <FRAME name="statuswindow" src="statuswindow.php" frameborder=0 marginheight=0 marginwidth=0>
-    <FRAME name="controlwindow" src="controlwindow.php" frameborder=0 marginheight=0 marginwidth=0>
-    <FRAME name="infowindow" src="infowindow.php">
-    <FRAME name="plotwindow" src="plotwindow.php">
-    <FRAME name="gainwindow" src="gain_reporter.php">
+  <FRAMESET COLS=1 ROWS="60px,95px,40px,110px,250px,*" border=1>
+    <FRAME name="banner" src="banner_new.php" frameborder=0 marginheight=0 marginwidth=0>
+    <FRAME name="statuswindow" src="status_window.php" frameborder=0 marginheight=0 marginwidth=0>
+    <FRAME name="controlpanel" src="control_panel.php" frameborder=0 marginheight=0 marginwidth=0>
+    <FRAME name="infowindow" src="">
+    <FRAME name="plotwindow" src="">
+    <FRAME name="gainwindow" src="">
   </FRAMESET>
-  <FRAMESET name=rightwindow COLS=1 ROWS="70px,*" border=0>
-    <FRAME name="logheader" src="logheader.php" frameborder=0 marginheight=0 marginwidth=0>
-    <FRAME name="logwindow" src="machine_status.php">
+  <FRAMESET name=rightwindow COLS=1 ROWS="70px,*" border=1>
+    <FRAME name="logheader" src="" frameborder=0 marginheight=0 marginwidth=0>
+    <FRAME name="logwindow" src="">
   </FRAMESET>
 </FRAMESET>
-</HTML>-->
+</html>
