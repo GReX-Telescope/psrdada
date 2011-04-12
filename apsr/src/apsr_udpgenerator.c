371cffbd9e1b02c66397b64c62a8881945ccca4b
Andrew Jameson <ajameson@users.sourceforge.net>
2011-04-12 22:52:28 +0000

    Changed default gains to 33, was 50

--- apsr/src/apsr_udpgenerator.c
+++ apsr/src/apsr_udpgenerator.c
@@ -55,8 +55,8 @@ double rand_normal(double mean, double stddev);
 /* UDP socket */
 struct sockaddr_in dagram_socket;
 
-int new_gain_value = 50;  // Standard gain of 50 % 
-int current_gain = 50;
+int new_gain_value = 33;  // Standard gain of 50 % 
+int current_gain = 33;
 
 int main(int argc, char *argv[])
 {
@@ -1238,8 +1238,8 @@ time_t wait_for_start(int daemon, multilog_t* log) {
       } else {
 
         time_t remainder = utc_start_time - time(0);
-        fprintf(sockout,  "parsed %s into %d current=%d starting in %d seconds\n",
-                utc_string, (int) utc_start_time, (int) time(0), (int) remainder);
+        //fprintf(sockout,  "parsed %s into %d current=%d starting in %d seconds\n",
+        //        utc_string, (int) utc_start_time, (int) time(0), (int) remainder);
         fprintf(sockout,"ok\n");
 
         sleep(remainder-1);
