898f56e7425999a230741019101e2f418c0f8c64
Andrew Jameson <ajameson@users.sourceforge.net>
2011-04-12 22:46:02 +0000

    Minor log change

--- apsr/scripts/server_apsr_sys_monitor.pl
+++ apsr/scripts/server_apsr_sys_monitor.pl
@@ -17,6 +17,7 @@ use Dada::server_sys_monitor qw(%cfg);
 #
 $Dada::server_sys_monitor::dl = 1;
 $Dada::server_sys_monitor::daemon_name = Dada::daemonBaseName($0);
+$Dada::server_sys_monitor::master_log_prefix = "nexus";
 $Dada::server_sys_monitor::log_host = $cfg{"SERVER_HOST"};
 $Dada::server_sys_monitor::log_port = $cfg{"SERVER_SYS_LOG_PORT"};
 
