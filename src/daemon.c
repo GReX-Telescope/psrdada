f66383668a94708e10a37e368b59ea8652047205
Andrew Jameson <ajameson@users.sourceforge.net>
2011-05-30 04:13:42 +0000

    Fixed issue when creating a daemon with a logfile for stdout/err

--- src/daemon.c
+++ src/daemon.c
@@ -11,50 +11,83 @@
 
 void be_a_daemon ()
 {
+  // fork off the parent process
   pid_t pid = fork();
 
   if (pid < 0)
     exit(EXIT_FAILURE);
   
+  // if the pid is good, then we can exit the parent process
   if (pid > 0)
     exit(EXIT_SUCCESS);
+
+  // change the file mode mask
+  umask (0);
   
-  /* Create a new SID for the child process */
+  // create a new SID for the child process
   if (setsid() < 0)
     exit (EXIT_FAILURE);
   
-  /* Change the current working directory */
+  // change the current working directory 
   if (chdir("/") < 0)
     exit (EXIT_FAILURE);
   
-  /* Close out the standard file descriptors */
+  // Close out the standard file descriptors
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
   close(STDERR_FILENO);
 }
 
 
-int be_a_daemon_with_log(char * logfile) {
+int be_a_daemon_with_log(char * logfile) 
+{
 
-  /* become a daemon */
-  be_a_daemon();
+  // fork off the parent process
+  pid_t pid = fork();
 
-  int fd = 0;
+  if (pid < 0)
+    exit(EXIT_FAILURE);
 
-  if ((fd = open("/dev/null", O_RDWR)) < 0) {   /* stdin */
+  // if the pid is good, then we can exit the parent process
+  if (pid > 0)
+    exit(EXIT_SUCCESS);
+
+  // change the file mode mask
+  umask (0);
+
+  // create a new SID for the child process
+  if (setsid() < 0)
+    exit (EXIT_FAILURE);
+
+  // change the current working directory 
+  if (chdir("/") < 0)
+    exit (EXIT_FAILURE);
+
+  // close STDIN and reopen it as /dev/null
+  close(STDIN_FILENO);
+  if (open("/dev/null", O_RDONLY) < 0) {
     return (PS_IOERR);
   }
 
-  /* redirect STDOUT and STDERR to the logfile */
-  if (logfile) {
-    if ((fd = open(logfile, O_CREAT|O_WRONLY|O_APPEND, 0666)) < 0)  /* stdout */
+  // if we are redirecting the STDERR and STDOUT to a logfile
+  if (logfile)
+  {
+    // open the logfile
+    int fd = open(logfile, O_RDWR|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR|S_IRGRP);
+    if (fd == -1) 
       return(PS_IOERR);
-  }
 
-  if (dup(fd) < 0) {        /* stderr */
-    return(PS_IOERR);
-  }
+    // dup2 will close STD[OUT|ERR]_FILENO if neccesary
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
 
+    close(fd);
+
+  } 
+  else // just close STDOUT and STDERR
+  {
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+  }
   return 0;
-
 }
