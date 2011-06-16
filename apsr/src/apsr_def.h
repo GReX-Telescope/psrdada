ab43bcb6f30555058b01bdfdde6b7a6076b6ff6d
Andrew Jameson <ajameson@users.sourceforge.net>
2011-06-16 23:48:10 +0000

    Changed some of the default port numbers

--- apsr/src/apsr_def.h
+++ apsr/src/apsr_def.h
@@ -5,24 +5,24 @@
 #define APSR_DEFAULT_UDPDB_PORT 10002
 
 /* default port to connect to udpdb statistics interface */
-#define APSR_DEFAULT_UDPDB_STATS  56060
+#define APSR_DEFAULT_UDPDB_STATS  26060
 
 /* default port to connect to dada_dbudp logging interface */
-#define APSR_DEFAULT_DBUDP_LOG  56064
+#define APSR_DEFAULT_DBUDP_LOG  26064
 
 /* ports for the udp generator */
-#define APSR_UDPGENERATOR_LOG          56066
-#define APSR_UDPGENERATOR_CTRL_PORT    52014
-#define APSR_UDPGENERATOR_GAIN_PORT    52015
+#define APSR_UDPGENERATOR_LOG          26066
+#define APSR_UDPGENERATOR_CTRL_PORT    22014
+#define APSR_UDPGENERATOR_GAIN_PORT    22015
 
 /* default port for udpdisk udp packets */
-#define APSR_DEFAULT_UDPDISK_PORT 59500
+#define APSR_DEFAULT_UDPDISK_PORT 29500
 
 /* default port for PWC control */
-#define APSR_DEFAULT_PWC_PORT     52022
+#define APSR_DEFAULT_PWC_PORT     22022
 
 /* default port for PWC multilog messages */
-#define APSR_DEFAULT_PWC_LOGPORT  52023
+#define APSR_DEFAULT_PWC_LOGPORT  22023
 
 
 
