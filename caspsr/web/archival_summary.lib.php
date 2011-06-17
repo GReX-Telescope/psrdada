aed322e576a278572f20bdd4bf5344d2c43d5813
Andrew Jameson <ajameson@users.sourceforge.net>
2011-06-17 01:14:36 +0000

    Added archival summary

---
+++ caspsr/web/archival_summary.lib.php
@@ -0,0 +1,122 @@
+<?PHP
+
+include("caspsr_webpage.lib.php");
+include("definitions_i.php");
+include("functions_i.php");
+include($instrument.".lib.php");
+
+class archival_summary extends caspsr_webpage 
+{
+
+  function archival_summary()
+  {
+    caspsr_webpage::caspsr_webpage();
+  }
+
+  function javaScriptCallback()
+  {
+    return "archival_summary_request();";
+  }
+
+  function printJavaScriptHead()
+  {
+    $inst = new caspsr();
+?>
+    <script type='text/javascript'>  
+
+      // handle the response from an archival_summary request
+      function handle_archival_summary_request( as_http_request) 
+      {
+        if ( as_http_request.readyState == 4) {
+          var response = String(as_http_request.responseText)
+
+          if (response.indexOf("Could not connect to") == -1) 
+          {
+
+            var lines = response.split(";;;");
+            var values;
+
+            for (i=0; i<lines.length; i++) 
+            {
+              values = lines[i].split(":::");
+              if (values[0]) 
+              {
+                if (document.getElementById(values[0])) {
+                  document.getElementById(values[0]).innerHTML = values[1];
+                }
+              }
+            }
+          }
+        }
+      }
+
+      // generate an obsevartaion info request
+      function archival_summary_request() 
+      {
+        var host = "<?echo $inst->config["SERVER_HOST"];?>";
+        var port = "<?echo $inst->config["SERVER_WEB_MONITOR_PORT"];?>";
+        var url = "archival_summary.lib.php?update=true&host="+host+"&port="+port;
+
+        if (window.XMLHttpRequest)
+          as_http_request = new XMLHttpRequest();
+        else
+          as_http_request = new ActiveXObject("Microsoft.XMLHTTP");
+
+        as_http_request.onreadystatechange = function() {
+          handle_archival_summary_request( as_http_request)
+        };
+        as_http_request.open("GET", url, true);
+        as_http_request.send(null);
+      }
+
+    </script>
+<?
+  }
+
+  /* HTML for this page */
+  function printHTML($divargs="") 
+  {
+    $this->openBlockHeader("Archival Summary", $divargs);
+?>
+    <table cellpadding=1 cellspacing=0 border=0>
+      <tr>
+        <th align="right">Failed</th>
+        <td align="left"><span id="failed"></span></td>
+      </tr>
+      <tr>
+        <th align="right">Finished</th>
+        <td align="left"><span id="finished"></span></td>
+      </tr>
+      <tr>
+        <th align="right">Transferred</th>
+        <td align="left"><span id="transferred"></span></td>
+      </tr>
+      <tr>
+        <th align="right">Deleted</th>
+        <td align="left"><span id="deleted"></span></td>
+      </tr>
+    </table>
+<?
+    $this->closeBlockHeader();
+  }
+
+  function printUpdateHTML($host, $port)
+  {
+
+    $timeout = 1;
+    list ($socket, $result) = openSocket($host, $port, $timeout);
+    if ($result == "ok") {
+
+      $bytes_written = socketWrite($socket, "archival_info\r\n");
+      $string = socketRead($socket);
+      socket_close($socket);
+
+    } else {
+      $string = "Could not connect to $host:$port<BR>\n";
+    }
+
+    echo $string;
+
+  }
+}
+handledirect("archival_summary");
--- caspsr/web/index.php
+++ caspsr/web/index.php
@@ -5,12 +5,14 @@ include("machine_summary.lib.php");
 include("state_update.lib.php");
 include("log_list.lib.php");
 include("plot_window.lib.php");
+include("archival_summary.lib.php");
 
 $state_update = new state_update();
 $current_observation = new current_observation();
 $machine_summary = new machine_summary();
 $log_list = new log_list();
 $plot_window = new plot_window();
+$archival_summary = new archival_summary();
 
 // get the required style sheets
 $css = array();
@@ -19,6 +21,7 @@ $css = array_merge($css, $current_observation->css);
 $css = array_merge($css, $machine_summary->css);
 $css = array_merge($css, $log_list->css);
 $css = array_merge($css, $plot_window->css);
+$css = array_merge($css, $archival_summary->css);
 
 // get the required external javascript 
 $ejs = array();
@@ -27,6 +30,7 @@ $ejs = array_merge($ejs, $current_observation->ejs);
 $ejs = array_merge($ejs, $machine_summary->ejs);
 $ejs = array_merge($ejs, $log_list->ejs);
 $ejs = array_merge($ejs, $plot_window->ejs);
+$ejs = array_merge($ejs, $archival_summary->ejs);
 
 $css = array_unique($css);
 $ejs = array_unique($ejs);
@@ -50,6 +54,7 @@ $ejs = array_unique($ejs);
       <? echo $plot_window->javaScriptCallback()."\n"; ?>
       <? echo $state_update->javaScriptCallback()."\n"; ?>
       <? echo $current_observation->javaScriptCallback()."\n";?>
+      <? echo $archival_summary->javaScriptCallback()."\n"; ?>
       <? echo $machine_summary->javaScriptCallback()."\n"; ?>
       <? echo $log_list->javaScriptCallback()."\n"; ?>
       setTimeout('poll_server()', 4000);
@@ -60,6 +65,7 @@ $ejs = array_unique($ejs);
   $plot_window->printJavaScriptHead();
   $state_update->printJavaScriptHead();
   $current_observation->printJavaScriptHead();
+  $archival_summary->printJavaScriptHead();
   $machine_summary->printJavaScriptHead();
   $log_list->printJavaScriptHead();
 ?> 
@@ -70,31 +76,44 @@ $ejs = array_unique($ejs);
   $plot_window->printJavaScriptBody();
   $state_update->printJavaScriptBody();
   $current_observation->printJavaScriptBody();
+  $archival_summary->printJavaScriptBody();
   $machine_summary->printJavaScriptBody();
   $log_list->printJavaScriptBody();
 ?>
-<div class="PageBackgroundSimpleGradient">
-</div>
-<div class="Main">
-  <div class="contentLayout">
-    <div class="sidebar1">
-      <div style='text-align: center; vertical-align: middle;'>
-        <img src="/caspsr/images/caspsr_logo_200x60.png" width=200 height=60>
-      </div>
-
+  <div class="PageBackgroundSimpleGradient">
+  </div>
+  <div class="Main">
+    <div class="contentLayout">
+      <div class="sidebar1">
+        <div style='text-align: center; vertical-align: middle;'>
+          <img src="/caspsr/images/caspsr_logo_200x60.png" width=200 height=60>
+        </div>
 <?
   $plot_window->printHTML();      
 ?>
-    </div><!-- sidebar1 -->
-  </div><!-- contentLayout -->
-  <div class="content">
-  <?
+      </div>
+    </div>
+    <div class="content">
+<?
   $state_update->printHTML();
-  $current_observation->printHTML();
+?> 
+      <div style='position: relative; overflow-x: hidden; overflow-y: hidden; margin: 0px; margin-right: 10px; min-height: 15px; min-width:15px; height: 100%;'>
+        <div style='position: relative; padding: 0; margin: 0; overflow-x: hidden; overflow-y: hidden; float: right; width: 200px; height: 100%;'>
+<?
+  $archival_summary->printHTML(" style='margin: 0px; margin-right: 10px;'");
+?>
+        </div>
+        <div style='position: relative; padding:0 margin: 0; overflow-x: hidden; overflow-y: hidden'>
+<?
+  $current_observation->printHTML(" style='margin: 0; margin-right: 10px;'");
+?>
+        </div>
+      </div>
+<?
   $machine_summary->printHTML();
   $log_list->printHTML();
-  ?>
+?>
+    </div>
   </div>
-</div>
 </body>
 </html>
