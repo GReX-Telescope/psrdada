0c9fbaf9f92e7a99acffa8532c712fe64969a355
Andrew Jameson <ajameson@users.sourceforge.net>
2016-10-23 23:10:41 +0000

    generalised to use the Dada::client_logger

--- bpsr/scripts/client_bpsr_src_logger.pl
+++ bpsr/scripts/client_bpsr_src_logger.pl
@@ -1,134 +1,60 @@
 #!/usr/bin/env perl
 
-#
-# Author:   Andrew Jameson
-# Created:  1 Feb 2008
-# Modified: 1 Feb 2008
-# 
-
-#
-# Include Modules
-#
-
 use lib $ENV{"DADA_ROOT"}."/bin";
 
-use Bpsr;           # BPSR/DADAModule for configuration options
-use strict;         # strict mode (like -Wall)
-use File::Basename;
-use Getopt::Std;
-
+use strict;
+use warnings;
+use Bpsr;
+use Dada::client_logger qw(%cfg);
 
 #
-# Constants
+# Function Prototypes
 #
-use constant DEBUG_LEVEL   => 1;
-use constant LOGFILE       => "bpsr_src_logger.log";
-
+sub usage();
 
 #
 # Global Variable Declarations
 #
-our %cfg : shared = Bpsr::getConfig();      # Bpsr.cfg in a hash
-our $log_socket;
-our $log_fh;
-our $pwc_id : shared = "";
+%cfg = Bpsr::getConfig();
 
 #
-# Local Variable Declarations
+# Initialize module variables
 #
-my $logfile = $cfg{"CLIENT_LOG_DIR"}."/".LOGFILE;
-my $type = "INFO";
-my $line = "";
-my $name = "proc";
+$Dada::client_logger::dl = 1;
+$Dada::client_logger::log_host = $cfg{"SERVER_HOST"};
+$Dada::client_logger::log_port = $cfg{"SERVER_SRC_LOG_PORT"};
+$Dada::client_logger::log_sock = 0;
+$Dada::client_logger::daemon_name = Dada::daemonBaseName($0);
+$Dada::client_logger::type = "src";
+$Dada::client_logger::daemon = "proc";
 
-#
-# Register Signal handlers
-#
-$SIG{INT} = \&sigHandle;
-$SIG{TERM} = \&sigHandle;
-$SIG{PIPE} = \&sigPipeHandle;
-
-# Auto flush output
+# Autoflush STDOUT
 $| = 1;
 
-if ($#ARGV != 1)
 {
-  usage();
-  exit(1);
-}
-$pwc_id  = $ARGV[0];
-$name     = $ARGV[1];
-
-my %opts;
-getopts('e', \%opts);
-
-if ($opts{e}) {
-  $type = "ERROR";
-}
-
-
-# Open a connection to the nexus logging facility
-$log_socket = Dada::nexusLogOpen($cfg{"SERVER_HOST"},$cfg{"SERVER_SRC_LOG_PORT"});
-if (!$log_socket) {
-  print "Could not open a connection to the nexus SRC log: $log_socket\n";
-}
-
-
-logMessage(2, "INFO", "STARTING src logging", $logfile);
-
-while (defined($line = <STDIN>)) {
-
-  chomp $line;
-  logMessage(0,$type,$line, $logfile);
-
-}
-
-logMessage(2, "INFO", "ENDING src logging", $logfile);
-
-exit 0;
-
-
-#
-# Logs a message to the Nexus
-#
-sub logMessage($$$$) {
-  (my $level, my $type, my $message, my $logfile) = @_;
-  if ($level <= DEBUG_LEVEL) {
-    my $time = Dada::getCurrentDadaTime();
-    if (!($log_socket)) {
-      $log_socket = Dada::nexusLogOpen($cfg{"SERVER_HOST"},$cfg{"SERVER_SRC_LOG_PORT"});
-    }
-    if ($log_socket) {
-      Dada::nexusLogMessage($log_socket, $pwc_id, $time, "src", $type, $name, $message);
-    }
-    open $log_fh, ">>".$logfile;
-    print $log_fh "[".$time."] ".$message."\n";
-    close $log_fh
-  }
-}
-
-sub sigHandle($) {
-
-  my $sigName = shift;
-  print STDERR basename($0)." : Received SIG".$sigName."\n";
-
-  if ($log_socket) {
-    close($log_socket);
+  # parse command line
+  if ($#ARGV != 1)
+  {
+    usage();
+    exit(1);
   }
 
-  print STDERR basename($0)." : Exiting\n";
+  $Dada::client_logger::pwc_id = $ARGV[0];
+  $Dada::client_logger::daemon = $ARGV[1];
 
-  exit 1;
+  my $result = 0;
+  $result = Dada::client_logger->main();
 
+  exit($result);
 }
 
-sub sigPipeHandle($) {
-
-  my $sigName = shift;
-  print STDERR basename($0)." : Received SIG".$sigName."\n";
-  $log_socket = 0;
-  $log_socket = Dada::nexusLogOpen($cfg{"SERVER_HOST"},$cfg{"SERVER_SRC_LOG_PORT"});
-
+########################################
+#
+#
+sub usage()
+{
+  print STDERR "Usage: ".Dada::daemonBaseName($0)." pwc_id tag\n";
+  print STDERR "   pwc_id    integer of the PWC\n";
+  print STDERR "   daemon    short name of program generating output\n";
 }
-
 
