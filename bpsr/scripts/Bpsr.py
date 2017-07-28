2578b5bff2031f2a3d359a1791aec6d2c5f14b94
Andrew Jameson <ajameson@users.sourceforge.net>
2017-07-28 04:57:23 +0000

    removed corr dependency and connection timeouts

--- bpsr/scripts/Bpsr.py
+++ bpsr/scripts/Bpsr.py
@@ -6,7 +6,7 @@
 
 import Dada, struct, math, median_smooth, time, matplotlib, pylab, os
 import sys, cStringIO, traceback
-import corr, numpy, fnmatch
+import numpy, fnmatch
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
@@ -49,25 +49,5 @@ def connectRoach(dl, rid):
   attempts = 0
 
   Dada.logMsg(2, dl, "["+rid+"] connectRoach: connecting to " + roach_ip + ":" + str(roach_port))
-
-  # connect to ROACH FPGA
-  while (not connected and attempts < 5):
-
-    Dada.logMsg(3, dl, "["+rid+"] connectRoach: connection attempt " + str(attempts) + " for " + roach_ip + ":" + str(roach_port))
-    fpga = corr.katcp_wrapper.FpgaClient(roach_ip, roach_port)
-    time.sleep(0.1)
-    if (fpga.is_connected()):
-      Dada.logMsg(3, dl, "["+rid+"] connectRoach: connected to " + roach_ip + ":" + str(roach_port))
-
-    connected = fpga.is_connected()
-
-    if (not connected):
-      Dada.logMsg(0, dl, "["+rid+"] connectRoach: connection to " + roach_ip + " failed, retrying")
-      time.sleep(1.0)
-      attempts += 1
-
-  if (not connected):
-    Dada.logMsg(-2, dl, "["+rid+"] connectRoach: connection failed")
-    return ("fail", 0)
 
   return ("ok", fpga)
