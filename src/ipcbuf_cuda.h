e4ab7a8fed5942c226430c7bd0902e4693bb57a5
Andrew Jameson <andrew.jameson@gmail.com>
2017-12-20 10:29:27 +1100

    typo

--- src/ipcbuf_cuda.h
+++ src/ipcbuf_cuda.h
@@ -12,7 +12,7 @@
 extern "C" {
 #endif
 
-/*! zero the next buffer in a IPBBUF using CUDA */
+/*! zero the next buffer in a IPCBUF using CUDA */
 ssize_t ipcbuf_zero_next_block_cuda (ipcbuf_t* id, char * dev_ptr, size_t dev_bytes, cudaStream_t stream);
 
 #ifdef __cplusplus
